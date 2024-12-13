d1e515b91ab46f3b407ef3821d63ef3640050871
meiliamyy <meiliarahmi8@gmail.com>
2024-12-13 22:50:59 +0700

    modified

--- 4.basketball-score-board/index.js
+++ 4.basketball-score-board/index.js
@@ -12,3 +12,11 @@ function increaseGuestScore(points) {
     guestScore += points;
     guestScoreEl.textContent = guestScore;
 }
+
+function resetAllScore() {
+    homeScore = 0;
+    guestScore = 0;
+    homeScoreEl.textContent = homeScore;
+    guestScoreEl.textContent = guestScore;
+}
+
