b3236e74c82fea0261020aa99a2e2a00a9e20627
meiliamyy <meiliarahmi8@gmail.com>
2024-12-18 23:02:36 +0700

    new modified project 8

--- 8. google-chrome-extension/index.js
+++ 8. google-chrome-extension/index.js
@@ -1,18 +1,37 @@
-let myLeads = ["www.awesomelead.com", "www.epiclead.com", "www.greatlead.com"]
-let inputEl = document.getElementById("input-el")
+let myLeads = []
+const inputEl = document.getElementById("input-el")
+const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
 
-function saveLead() {
-    // console.log("Button Clicked from onclick attribute!")
-}
-let inputBtn = document.getElementById("input-btn")
+// function saveLead() {
+//     // console.log("Button Clicked from onclick attribute!")
+// }
+
 
 inputBtn.addEventListener("click", function() {
     myLeads.push(inputEl.value)
     // console.log("Button clicked from addEventListerner")
+    inputEl.value = ""
+    renderLeads()
 })
 
-for (let i = 0; i < myLeads.length; i++) {
-    ulEl.textContent += myLeads[i] + " "
 
+function renderLeads () {
+    let listItems = ""
+    for (let i = 0; i < myLeads.length; i++) {
+        // listItems += "<li><a target='_blank' href='" + myLeads[i] + "'>" + myLeads[i] + "</a></li>";
+
+        listItems += `
+            <li>
+                <a target='_blank' href='${myLeads[i]}'>
+                    ${myLeads[i]}
+                </a>
+            </li>
+        `
+    }
+    ulEl.innerHTML = listItem
 }
+        
+
+
+
