61bb6e85b8d1a652780adb58f516fb71e8a4cac3
meiliamyy <meiliarahmi8@gmail.com>
2024-12-30 21:41:05 +0700

    modified project 8 and add new project 14

--- 8. google-chrome-extension/index.js
+++ 8. google-chrome-extension/index.js
@@ -1,34 +1,47 @@
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.1.0/firebase-app.js"
+import { getDataBase } from "https://www.gstatic.com/firebasejs/11.1.0/firebase-database.js"
+
+const firebaseConfig = {
+    databaseURl: "https://leads-tracker-app-78cdb-default-rtdb.asia-southeast1.firebasedatabase.app/"
+}
+
+const app = initializeApp(firebaseConfig)
+
+const database = getDataBase(app)
+
 let myLeads = []
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
+const deleteBtn = document.getElementById("delete-btn")
+const tabBtn = document.getElementById("tab-btn")
 const ulEl = document.getElementById("ul-el")
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
 
-// localStorage.setItem("myName", "Meilia")
-// let name = localStorage.getItem("myName")
-// console.log(name)
+if (leadsFromLocalStorage) {
+    myLeads = leadsFromLocalStorage
+    renderLeads()
+}
 
-// localStorage.clear()
-
-
-// function saveLead() {
-//     // console.log("Button Clicked from onclick attribute!")
-// }
-
+deleteBtn.addEventListener("dblclick", function() {
+    localStorage.clear()
+    myLeads = []
+    renderLeads()
+})
 
 inputBtn.addEventListener("click", function() {
     myLeads.push(inputEl.value)
     // console.log("Button clicked from addEventListerner")
     inputEl.value = ""
     localStorage.setItem("myLeads", JSON.stringify(myLeads))
-    // renderLeads()
+    renderLeads()
 })
 
 
-function renderLeads () {
+function renderLeads() {
     let listItems = ""
     for (let i = 0; i < myLeads.length; i++) {
         // listItems += "<li><a target='_blank' href='" + myLeads[i] + "'>" + myLeads[i] + "</a></li>";
-
         listItems += `
             <li>
                 <a target='_blank' href='${myLeads[i]}'>
@@ -39,7 +52,26 @@ function renderLeads () {
     }
     ulEl.innerHTML = listItems
 }
-  
+
+const tabs  = [
+    {url: "https://www.instagram.com"}
+]
+
+
+tabBtn.addEventListener("click", function(){
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        // since only one tab should be active and in the current window at once
+        // the return variable should only have one entry
+        let activeTab = tabs[0]
+        let activeTabId = activeTab.id // or do whatever you need
+
+    })
+    
+    myLeads.push(tabs[0].url)
+    localStorage.setItem("myLeads", JSON.stringify(myLeads) )
+    render(myLeads)
+    
+})
 
 
 
