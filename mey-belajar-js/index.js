6dc12015f46c5ac5d618d5d7357115571620639c
meiliamyy <meiliarahmi8@gmail.com>
2025-01-19 16:26:29 +0700

    modified learn javascript

---
+++ mey-belajar-js/index.js
@@ -0,0 +1,47 @@
+//buat sum of first N number
+sum = 0;
+for (let number = 1; number <= 10; number++) {
+  sum = sum + number;
+}
+console.log(sum);
+
+//buat bilangan genap dari 1-10 dengan if dan loop
+for (let number = 1; number <= 10; number++) {
+  if (number % 2 === 0) {
+    console.log(number);
+  }
+}
+
+//buat bilangan genap dari 10-1
+for (let number = 10; (number <= 10) & (number >= 1); number--) {
+  console.log(number);
+}
+
+//buat loop ketika ada spasi dalam kata
+word = "Fadil Risdian Ansori";
+
+for (let i = 0; i < word.length; i++) {
+  if (word[i] === " ") {
+    console.log(word[i]);
+    word[i] = "";
+  }
+}
+
+// buat arry isinya 5 peralan dapur
+name = "Meilia Alvi Rahmi";
+nameSplit = name.split(" ");
+// console.log(nameSplit)
+
+for (let i = 0; i < nameSplit.length; i++) {
+  // consolzse.log(nameSplit[i])
+}
+
+for (let i = 10; i >= 1; i--) {
+  console.log(i);
+}
+
+alatDapur = ["pisau", "kompor", "wajan", "sodet", "panci"];
+
+for (let i = 0; i < alatDapur.length; i++) {
+  console.log(alatDapur[i]);
+}
