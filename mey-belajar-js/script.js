17835a676f1eb2214e722d51cae822778b545f2b
meiliamyy <meiliarahmi8@gmail.com>
2025-02-01 20:50:41 +0700

    add new belajar js map, filter, reduce) git status clear clear()

---
+++ mey-belajar-js/script.js
@@ -0,0 +1,42 @@
+// ambil semua elemen video dengan DOM
+const videos = Array.from(document.querySelectorAll("[data-duration]"));
+// console.log(videos);
+
+// saring elemen javascript lanjutan dari semua elemen video
+
+const javascriptLanjutan = videos
+  .filter((videos) => videos.textContent.includes("JAVASCRIPT LANJUTAN"))
+
+  // ambil durasi masing masing video
+
+  .map((videos) => videos.dataset.duration)
+
+  // ubah durasi menjadi int, ubah menit ke detik
+  .map((waktu) => {
+    const parts = waktu.split(":").map((part) => parseFloat(part));
+    return parts[0] * 60 + parts[1];
+  })
+
+  .reduce((total, detik) => total + detik);
+
+// kemudian ubah format menjadi jam menit detik
+const jam = Math.floor(javascriptLanjutan / 3600);
+const menit = Math.floor((javascriptLanjutan % 3600) / 60);
+const detik = javascriptLanjutan % 60;
+
+console.log(jam, menit, detik);
+// simpan ke DOM
+
+const pDurasi = document.querySelector(".total-durasi");
+
+// console.log(pDurasi);
+
+pDurasi.textContent = `${jam} jam, ${menit} menit, ${detik} detik`;
+
+// console.log(javascriptLanjutan);
+
+const jmlVideo = videos.filter((videos) => videos.textContent.includes("JAVASCRIPT LANJUTAN")).length;
+
+const pJmlVideo = document.querySelector(".jumlah-video");
+
+pJmlVideo.textContent = `${jmlVideo} Video`;
