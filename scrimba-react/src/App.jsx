39b3a8cdd47d9baee6c6534ca5003378461e21fe
meiliamyy <meiliarahmi8@gmail.com>
2025-02-12 22:52:17 +0700

    add learn react of Scrimba

---
+++ scrimba-react/src/App.jsx
@@ -0,0 +1,9 @@
+import { createRoot } from "react-dom/client"
+
+createRoot(document.getElementById("root")).render(
+    <ul>
+    <li>Super popular JS library</li>
+    <li>Will help me be even more employable</li>
+    <li>sfsadtg has a pretty cool logo</li>
+    </ul>
+)
