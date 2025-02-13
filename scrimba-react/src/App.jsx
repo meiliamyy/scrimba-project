5a3d7e34e4da5c019fc86abb47af1fa0255c5edd
meiliamyy <meiliarahmi8@gmail.com>
2025-02-13 21:21:22 +0700

    add new learn react

--- scrimba-react/src/App.jsx
+++ scrimba-react/src/App.jsx
@@ -1,11 +1,3 @@
-import { createRoot } from "react-dom/client"
-
-const root = createRoot(document.getElementById("root"))
-root.render(
-    <ul>
-    <li>Super popular JS library</li>
-    <li>Will help me be even more employable</li>
-    <li>sfsadtg has a pretty cool logo</li>
-    li
-    </ul>
-)
+export default function App() {
+  return <h1>Hello, World!</h1>;
+}
