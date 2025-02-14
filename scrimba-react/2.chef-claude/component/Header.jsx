9c4a2e466b11d036d705108eeb61458be81d8244
meiliamyy <meiliarahmi8@gmail.com>
2025-02-14 22:54:27 +0700

    add project 2.chef-claude

---
+++ scrimba-react/2.chef-claude/component/Header.jsx
@@ -0,0 +1,8 @@
+export default function Header() {
+  return (
+    <header>
+      <img src="../chef-claude-icon.png" alt="icon chef claude" />
+      <h1>Chef Claude</h1>
+    </header>
+  );
+}
---
+++ scrimba-react/2.chef-claude/src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import Header from "../component/Header";
+import Form from "../component/form";
+
+export default function App() {
+  return (
+    <>
+      <Header />
+      <Form />
+    </>
+  );
+}
