5dffb3414eddaefd6301bb02ed13fdba19511363
meiliamyy <meiliarahmi8@gmail.com>
2025-02-24 15:32:09 +0700

    add modified Influencer-Scrapping

--- mey-plan-react/Influencer-Scrapping/src/ArtistCard.jsx
+++ mey-plan-react/Influencer-Scrapping/src/ArtistCard.jsx
@@ -4,7 +4,9 @@ import "./ArtistCard.css";
 const ArtistCard = ({ artist }) => {
   return (
     <div className="artist-card">
-      <img src={artist.photo} alt={artist.name} className="artist-photo" />
+      <div className="artist-photo-container">
+        <img src={artist.photo} alt={artist.name} className="artist-photo" />
+      </div>
       <h3>{artist.name}</h3>
       <p>Sentimen Positif: {artist.sentimentPositive}</p>
       <p>Sentimen Negatif: {artist.sentimentNegative}</p>
--- mey-plan-react/Influencer-Scrapping/src/SortButton.jsx
+++ mey-plan-react/Influencer-Scrapping/src/SortButton.jsx
@@ -1,10 +1,15 @@
 import React from "react";
+import "./SortButton.css";
 
 const SortButtons = ({ onSortPositive, onSortNegative }) => {
   return (
     <div className="sort-buttons">
-      <button onClick={onSortPositive}>Sort by Sentiment Positive (High to Low)</button>
-      <button onClick={onSortNegative}>Sort by Sentiment Negative (High to Low)</button>
+      <button className="btn" onClick={onSortPositive}>
+        Sort by Sentiment Positive (High to Low)
+      </button>
+      <button className="btn" onClick={onSortNegative}>
+        Sort by Sentiment Negative (High to Low)
+      </button>
     </div>
   );
 };
