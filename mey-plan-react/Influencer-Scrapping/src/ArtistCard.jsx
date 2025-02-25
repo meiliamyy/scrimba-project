4ab4fae73ac4caaf543f6819e946bb9721bc9961
meiliamyy <meiliarahmi8@gmail.com>
2025-02-25 15:27:19 +0700

    addmodified Influencer-Scrapping

--- mey-plan-react/Influencer-Scrapping/src/ArtistCard.jsx
+++ mey-plan-react/Influencer-Scrapping/src/ArtistCard.jsx
@@ -3,13 +3,17 @@ import "./ArtistCard.css";
 
 const ArtistCard = ({ artist }) => {
   return (
-    <div className="artist-card">
-      <div className="artist-photo-container">
-        <img src={artist.photo} alt={artist.name} className="artist-photo" />
+    <div className="card">
+      <div className="card2">
+        <div className="artist-photo-container">
+          <img src={artist.photo} alt={artist.name} className="artist-photo" />
+        </div>
+        <div className="artist-info">
+          <h3>Artist Name: {artist.name}</h3>
+          <p>Sentimen Positif: {artist.sentimentPositive}</p>
+          <p>Sentimen Negatif: {artist.sentimentNegative}</p>
+        </div>
       </div>
-      <h3>{artist.name}</h3>
-      <p>Sentimen Positif: {artist.sentimentPositive}</p>
-      <p>Sentimen Negatif: {artist.sentimentNegative}</p>
     </div>
   );
 };
